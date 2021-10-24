2deecf4fb13e9262b0a74aa2c9d5e5f047f20906
Luis Cruz <luisccc99@gmail.com>
2021-10-24 12:37:55 -0600

    add custom palette colors for app's theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import theme from './colors/App.theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <h1>Concert Recomendator App</h1>
+          <Button
+            size="large"
+            onClick={() => console.log('click')}
+            variant="contained">Start</Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
+
 
 export default App;
---
+++ src/colors/App.theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#546386',
+            main: '#283959',
+            dark: '#001330'
+        },
+        secondary: {
+            light: '#bfd4f2',
+            main: '#8ea3bf',
+            dark: '#60748f'
+        }
+    }
+});
+
+export default theme;
